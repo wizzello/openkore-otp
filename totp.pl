6c47a476fc8a6d70028834b7b4099ef6cacd42fc
Wizzello <owizzello@gmail.com>
2025-06-16 22:41:59 -0300

    fix: resolved send issues and working flow

--- totp.pl
+++ totp.pl
@@ -1,6 +1,6 @@
 #############################################################################
 #
-# TOTP Authentication plugin by wizzello
+# TOTP Authentication plugin by wizzello and pogramos
 #
 # Openkore: http://openkore.com/
 #
@@ -9,37 +9,51 @@
 #
 #############################################################################
 
-package totp;
+package tOTP;
 
 use strict;
 
+# Load necessary OpenKore modules
 use Plugins;
 use Globals;
 use Utils;
-use Log qw(message error);
 use Digest::SHA qw(hmac_sha1);
+use Log qw(message error);
 
-use Network::Send;
-
+# Register the plugin and provide unload handler
 Plugins::register(
-    'totpAuthentication',
-    'Adds TOTP login Authentication',
-    \\&unload
+    'tOTP',
+    'Handles OTP requests by generating and sending TOTP',
+    \&unload
 );
 
-# Global variable to store the login token received from server
-my $login_token = '';
-my $login_type = 0;
-
-# Hook to intercept TOTP request (opcode 0x0AE3)
-# You need to add the following packet to packet_list
-# '0AE3' => ['received_login_token', 'v l Z20 Z*', [qw(len login_type flag login_token)]],
+# Add hook to listen for the custom OTP request event
+# This event must be triggered by a modified OpenKore source (see README.md)
 my $hooks = Plugins::addHooks(
-    ['packet/received_login_token', \&onReceivedLoginToken]
+    ['totp/request_otp', \&on_request_otp]
 );
 
-# Generate OTP using SHA1
+# This function is called when OpenKore requests an OTP code.
+# It generates the TOTP code and sends it to the server.
+sub on_request_otp {
+    my (undef, $sender) = @_;
+
+    if (!$config{otpSeed}) {
+        error "[tOTP] ERROR: otpSeed is not set in config.txt\n";
+        return;
+    }
+
+    my $totp = generate_totp($config{otpSeed});
+    message "[tOTP] Generated TOTP: $totp\n";
+
+    my $packet = pack('v a6 C', 0x0C23, $totp, 0x00);
+
+    $sender->sendToServer($packet);
+    message "[tOTP] TOTP sent successfully\n";
+}
+
 sub decode_base32 {
+    # Converts a Base32 string (your OTP seed) into raw bytes
     my ($b32) = @_;
     $b32 = uc($b32);
     $b32 =~ tr/A-Z2-7//cd;
@@ -66,12 +80,14 @@ sub decode_base32 {
     return $bytes;
 }
 
+# Generate a TOTP code based on the secret seed
 sub generate_totp {
     my($secret, $time_step, $digits) = @_;
+    # Default time step is 30 seconds and code length is 6 digits
     $time_step ||= 30;
     $digits ||= 6;
 
-    my $decoded_key = decode_base32($secret);
+    my $decoded_key = decodeBase32($secret);
     my $tstamp = int(time() / $time_step);
     my $tbytes = pack('J>', $tstamp);
 
@@ -85,73 +101,10 @@ sub generate_totp {
     return $otp;
 }
 
-sub onReceivedLoginToken {
-    my (undef, $args) = @_;
-
-    message "[totp] Detected TOTP request packet (0x0AE3)\n";
-
-    # Extract information from the packet
-    my $packet_len = $args->{len} || 0;
-    $login_type = $args->{login_type} || 0;
-    my $flag = $args->{flag} || 0;
-    $login_token = $args->{login_token} || '';
-
-    message "[totp] Packet length: $packet_len, Login type: $login_type, Flag: $flag\n";
-
-    # Check if this is a TOTP request (typically when login_token is empty or minimal)
-    if ($packet_len == 28 && !$login_token) {
-        message "[totp] TOTP request detected - generating and sending TOTP code\n";
-
-        unless ($config{'totpSecret'}) {
-            error "[totp] Error: 'totpSecret' not configured in config.txt\n";
-            return;
-        }
-
-        # Generates the TOTP code 
-        # (Initially using Authen OATH, but in the future it may be a custom implementation)
-        my $totp = generate_totp($config{'totpSecret'});
-        unless (defined $totp) {
-            error "[totp] Failed to generate TOTP\n";
-            return;
-        }
-
-        message "[totp] Generated TOTP: $totp\n";
-
-        sendTOTPCode($totp);
-    }
-
-    # Overrides default packet processing to avoid duplication
-    return 0;
-}
-
-sub sendTOTPCode {
-    my ($totp) = @_;
-    
-    # Validate TOTP code format
-    unless ($totp =~ /^\d{6}$/) {
-        error "[totp] Invalid TOTP code format: $totp\n";
-        return;
-    }
-    
-    message "[totp] Sending TOTP code: $totp\n";
-    
-    # Create the packet based on your network capture
-    # Opcode 0x0C23 + TOTP code as ASCII string + null terminator
-    my $packet = pack('v', 0x0C23) . $totp . "\x00";
-    
-    # Send using OpenKore's network system
-    if ($net && $net->getState() == Network::IN_GAME || $net->getState() == Network::CONNECTED_TO_LOGIN_SERVER) {
-        $net->send($packet);
-        message "[totp] TOTP packet sent successfully\n";
-    } else {
-        error "[totp] Cannot send TOTP - not connected to server\n";
-    }
-}
-
 sub unload {
     # Unregister hooks when unloading the plugin
     Plugins::delHooks($hooks);
-    message "[totp] Plugin unloaded.\n";
+    message "[tOTP] Plugin unloaded.\n";
 }
 
 1;
